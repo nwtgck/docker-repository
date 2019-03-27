5df3d65ea4b59acf40775dc79c6cc087b3272b35
Ryo Ota <nwtgck@gmail.com>
2019-03-27 20:58:10 +0900

    [change] Add Travis CI job URL to release message

--- build.rb
+++ build.rb
@@ -9,10 +9,14 @@ Dir.chdir(dir_path){
   system("pwd")
   system("bash", "-xue", "build.bash")
   # TODO: Check whether dist exists
+  # Travis Job URL
+  travis_job_url = "https://travis-ci.com/nwtgck/docker-repository/jobs/#{ENV["TRAVIS_JOB_ID"]}"
+  # Release message
+  release_message = "Travis CI Job: #{travis_job_url}"
   # Create a tag
-  git_tag = "#{repo_name}/dummy_commit"
-  system("ghr", git_tag, "dist")
+  git_tag = "#{repo_name}/#{ENV["TRAVIS_COMMIT"]}"
+  # Publish to GitHub Release
+  system("ghr", "-b", release_message, git_tag, "dist")
+  # Print a message
+  puts("Released as tag '#{git_tag}'!")
 }
-
-# # Move dist to project dist to prepare publish
-# system("mv", File.join(dir_path, "dist"), "dist")
