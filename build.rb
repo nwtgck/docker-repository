54daf59126278d8ef92651aaf0be0128063c9c6b
Ryo Ota <nwtgck@gmail.com>
2019-03-27 21:06:58 +0900

    [change] Calculate SHA256 of files in dist

--- build.rb
+++ build.rb
@@ -9,10 +9,24 @@ Dir.chdir(dir_path){
   system("pwd")
   system("bash", "-xue", "build.bash")
   # TODO: Check whether dist exists
+  files_sha256 = ""
+  Dir.chdir("dist"){
+    # Calculate SHA256 of files
+    files_sha256 = `shasum -a 256 *`
+    # Print SHAN256
+    puts("SHA256:")
+    puts(files_sha256)
+    # Save as file in dist
+    File.write("FILES_SHA256.txt", files_sha256)
+  }
   # Travis Job URL
   travis_job_url = "https://travis-ci.com/nwtgck/docker-repository/jobs/#{ENV["TRAVIS_JOB_ID"]}"
   # Release message
-  release_message = "Travis CI Job: #{travis_job_url}"
+  release_message = <<EOS
+Travis CI Job: #{travis_job_url}
+SHA256:
+#{files_sha256}
+EOS
   # Create a tag
   git_tag = "#{repo_name}/#{ENV["TRAVIS_COMMIT"]}"
   # Publish to GitHub Release
