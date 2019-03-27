02d078876a825b22ff71392b8d399dfdd2fc02d6
Ryo Ota <nwtgck@gmail.com>
2019-03-27 20:47:28 +0900

    [change] Add GitHub Release publish configuration

--- build.rb
+++ build.rb
@@ -1,7 +1,18 @@
+# Requirements
+# * ghr: https://github.com/tcnksm/ghr
+
 # TODO: Hard code
-dir_path = "repos/nwtgck/piping-server"
+repo_name="nwtgck/piping-server"
+dir_path = File.join("repos", repo_name)
 
 Dir.chdir(dir_path){
   system("pwd")
   system("bash", "-xue", "build.bash")
+  # TODO: Check whether dist exists
+  # Create a tag
+  git_tag = "#{repo_name}/dummy_commit"
+  system("ghr", git_tag, "dist")
 }
+
+# # Move dist to project dist to prepare publish
+# system("mv", File.join(dir_path, "dist"), "dist")
